7d9318a9db7a234927585625f63a205de1890871
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-09-23 10:43:29 -0600

    Wrote complete skeleton for cloc.sh

--- cloc.sh
+++ cloc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# wget https://api.github.com/users/desired_user/repos
-# grep this_word repos
-
-# Here down is taken from
-# https://stackoverflow.com/questions/26881441/can-you-get-the-number-of-lines-of-code-from-a-github-repository
-git clone --depth 1 "$1" temp-linecount-repo &&
-printf "('temp-linecount-repo' will be deleted automatically)\n\n\n" &&
-cloc temp-linecount-repo &&
-rm -rf temp-linecount-repo
+wget https://api.github.com/users/Kardbord/repos
+grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// > strippedRepos.txt
+mkdir ./cloned_repos
+pushd ./cloned_repos
+# loop through file and clone all repos
+# printf "(Cloned repos will be deleted automatically)"\n\n\n"
+# cloc .
+# popd
+# rm -rf cloned_repos/
