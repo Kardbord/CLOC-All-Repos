974c8e836d9bdd2bd80577c581ec561cdd1c2932
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-09-26 20:41:49 -0600

    Added exclude flag to ignore xml files

--- cloc.sh
+++ cloc.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-wget https://api.github.com/users/Kardbord/repos
-grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// | sed s/\"//g > strippedRepos.txt
-mkdir ./cloned_repos
-pushd ./cloned_repos
-# loop through file and clone all repos
-while read repo; do
-    git clone $repo
-    #echo $repo
+wget https://api.github.com/users/Kardbord/repos 
+grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// | sed s/\"//g > strippedRepos.txt 
+mkdir ./cloned_repos 
+pushd ./cloned_repos 
+# loop through file and clone all repos 
+while read repo;
+do
+git clone $repo
+#echo $repo
 done < ../strippedRepos.txt
-printf "(Cloned repos will be deleted automatically)\n\n\n"
-cloc .
-popd
-rm -rf cloned_repos/
-rm strippedRepos.txt
+printf "(Cloned repos will be deleted automatically)\n\n\n" 
+cloc . --exclude-ext=xml 
+popd 
+rm -rf cloned_repos/ 
+rm strippedRepos.txt 
 rm repos
