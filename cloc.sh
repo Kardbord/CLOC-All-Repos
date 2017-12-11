4941e91062a2f8bb3d3b2f3700549f99218ee200
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2017-12-11 15:26:46 -0700

    Ran code through a validator and applied fixes for warnings

--- cloc.sh
+++ cloc.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-if [ -z "$1" ]
+if [ -z "$1" ];
 then
     echo "No arguments supplied. Provide a GitHub username."
     exit 1
 fi
-wget https://api.github.com/users/${1}/repos 
-grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// | sed s/\"//g > strippedRepos.txt 
-mkdir ./cloned_repos 
-pushd ./cloned_repos 
-# loop through file and clone all repos 
-while read repo;
+
+wget https://api.github.com/users/"${1}"/repos
+grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// | sed s/\"//g > strippedRepos.txt
+mkdir ./cloned_repos
+pushd ./cloned_repos || exit
+
+# loop through file and clone all repos
+while read -r repo;
 do
-    git clone $repo
+    git clone "$repo"
     #echo $repo
 done < ../strippedRepos.txt
-printf "(Cloned repos will be deleted automatically)\n\n\n" 
-cloc . --exclude-ext=xml 
-popd 
-rm -rf cloned_repos/ 
-rm strippedRepos.txt 
+
+printf "(Cloned repos will be deleted automatically)\\n\\n\\n"
+
+cloc . --exclude-ext=xml
+
+popd || exit
+rm -rf cloned_repos/
+rm strippedRepos.txt
 rm repos
