47ee47cb16db93b38ea2579f4fe72230687827dc
TannerKvarfordt <tanner.kvarfordt@gmail.com>
2019-11-03 14:17:23 -0700

    Cleaned things up

--- cloc.sh
+++ cloc.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
+
+function fail {
+    echo "$0 failed for some reason"
+    exit 1
+}
+
 if [ -z "$1" ];
 then
     echo "No arguments supplied. Provide a GitHub username."
     exit 1
 fi
 
-wget https://api.github.com/users/"${1}"/repos
-grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// | sed s/\"//g > strippedRepos.txt
-mkdir ./cloned_repos
-pushd ./cloned_repos || exit
+# Create a temporary directory to work in
+TEMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'cloc_tmp')
 
-# loop through file and clone all repos
-while read -r repo;
-do
+# Make sure our workspace gets cleaned up on exit
+trap 'rm -rf $TEMP_DIR' EXIT
+
+if [ ! -d "$TEMP_DIR" ]; then
+    echo "Failed to create temporary directory, quitting..."
+    exit 1
+fi
+
+pushd "$TEMP_DIR" > /dev/null 2>&1 || fail
+
+if ! wget https://api.github.com/users/"${1}"/repos > /dev/null 2>&1
+then
+    echo "Failed to access the GitHub API page for GitHub user $1."
+    exit 1
+fi
+
+grep clone_url repos | tr -s " " | cut -d" " -f3 | sed s/,// | sed s/\"//g > "$TEMP_DIR/strippedRepos.txt"
+
+while read -r repo; do
     git clone "$repo"
-    #echo $repo
-done < ../strippedRepos.txt
+done < "$TEMP_DIR/strippedRepos.txt"
 
-printf "(Cloned repos will be deleted automatically)\\n\\n\\n"
+echo -e "\n(Cloned repos will be deleted automatically)\n\n\n"
 
-cloc . --exclude-ext=xml
+echo -e "Counting lines of code...\n"
 
-popd || exit
-rm -rf cloned_repos/
-rm strippedRepos.txt
-rm repos
+#cloc . --exclude-ext=xml
+#cloc . --exclude-ext=json
+cloc .
+
+popd > /dev/null 2>&1 || fail
+
